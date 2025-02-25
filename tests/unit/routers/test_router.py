7a32194ce5e89b870916b7b0b25aae188ec6319a
Chris Mutel <cmutel@gmail.com>
2025-02-25 14:56:22 +0100

    Fix testing

--- tests/unit/routers/test_router.py
+++ tests/unit/routers/test_router.py
@@ -1,9 +1,10 @@
 import pytest
 
 from fastapi import HTTPException
+from starlette.requests import Request
 
-from py_semantic_taxonomy.adapters.routers.router import router
-from py_semantic_taxonomy.domain.entities import ConceptNotFoundError
+import py_semantic_taxonomy.adapters.routers.router as router
+from py_semantic_taxonomy.domain.entities import ConceptNotFoundError, Concept
 
 
 async def test_get_concept(mock_kos_graph, graph_service, entities):
@@ -21,3 +22,24 @@ async def test_get_concept_not_found(mock_kos_graph, graph_service):
         await router.get_concept("foo", service=graph_service)
     assert exc_info.value.status_code == 404
     assert exc_info.value.detail == "Concept with iri 'foo' not found"
+
+
+@pytest.mark.skip(reason="Needs to be integration test because of URL reverse needs FastAPI app")
+async def test_concept_redirect(mock_kos_graph, graph_service, entities):
+    mock_kos_graph.get_object_type.return_value = Concept
+
+    response = await router.generic_get_from_iri(
+        request=Request(scope={
+            "type": "http",
+            "path": "/concept/foo",
+            "method": "GET",
+            "scheme": "https",
+            "client": ("127.0.0.1", 8080),
+            "server": ("www.example.com", 443),
+            "headers": {},
+        }),
+        _="",
+        service=graph_service
+    )
+    assert response.status_code == 307
+    assert response.url == "https://www.example.com/concept/foo"
