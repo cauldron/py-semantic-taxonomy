6463b4962210e6d50f743b27d694edf933d190ba
Chris Mutel <cmutel@gmail.com>
2025-02-28 20:28:18 +0100

    Use postgres for integration tests

--- src/py_semantic_taxonomy/cfg.py
+++ src/py_semantic_taxonomy/cfg.py
@@ -3,6 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     db_backend: str = "postgres"
+    db_user: str = "missing"
+    db_pass: str = "missing"
+    db_host: str = "localhost"
+    db_port: int = 5432
+    db_name: str = "PyST"
 
     # allow_origins: Set[str] = {
     #     "https://brightway.cauldron.ch",
--- tests/integration/test_concept_api.py
+++ tests/integration/test_concept_api.py
@@ -1,4 +1,4 @@
-def test_get_concept(sqlite, cn_db, cn, client):
+def test_get_concept(postgres, cn_db, cn, client):
     response = client.get(
         "/concept/", params={"iri": "http://data.europa.eu/xsp/cn2024/010011000090"}
     )
@@ -13,6 +13,6 @@ def test_get_concept(sqlite, cn_db, cn, client):
         assert given[key] == value
 
 
-def test_get_concept_404(sqlite, cn_db, cn, client):
+def test_get_concept_404(postgres, cn_db, cn, client):
     response = client.get("/concept/", params={"iri": "http://data.europa.eu/xsp/cn2024/woof"})
     assert response.status_code == 404
