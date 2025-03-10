6358b922f97a897bd8100ccbd5f54aa66fe38d8c
Chris Mutel <cmutel@gmail.com>
2025-03-10 21:44:42 +0100

    Add interface tests with Starlette client

---
+++ tests/unit/routers/concepts/test_concept_router_interfaces.py
@@ -0,0 +1,25 @@
+from unittest.mock import AsyncMock
+
+from py_semantic_taxonomy.adapters.routers.router import GraphService
+from py_semantic_taxonomy.domain.entities import Concept
+
+
+async def test_create_concept(cn, client, monkeypatch):
+    monkeypatch.setattr(
+        GraphService, "create_concept", AsyncMock(return_value=Concept.from_json_ld(cn.concept_low))
+    )
+
+    response = client.post("/concept/", json=cn.concept_low)
+    assert response.status_code == 200
+
+
+async def test_create_concept_error(cn, client, monkeypatch):
+    monkeypatch.setattr(GraphService, "create_concept", AsyncMock())
+
+    obj = cn.concept_low
+    del obj["@id"]
+
+    response = client.post("/concept/", json=obj)
+    assert response.json()["detail"][0]["type"] == "missing"
+    assert response.json()["detail"][0]["loc"] == ["body", "@id"]
+    assert response.status_code == 422
