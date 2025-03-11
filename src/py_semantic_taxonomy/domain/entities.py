215e153f6cc218f32b0950bcd27c8742bee9d459
Chris Mutel <cmutel@gmail.com>
2025-03-11 16:31:34 +0100

    Add ConceptScheme domain entities

--- src/py_semantic_taxonomy/domain/entities.py
+++ src/py_semantic_taxonomy/domain/entities.py
@@ -1,9 +1,10 @@
 from copy import copy
 from dataclasses import asdict, dataclass, field
+from datetime import datetime
 
 from py_semantic_taxonomy.domain.constants import SKOS, SKOS_RELATIONSHIP_PREDICATES
 
-CONCEPT_MAPPING = {
+SKOS_MAPPING = {
     "id_": "@id",
     "types": "@type",
     "schemes": f"{SKOS}inScheme",
@@ -18,16 +19,14 @@ CONCEPT_MAPPING = {
 }
 
 
-@dataclass
-class Concept:
+# Allow mixing non-default and default values in dataclasses
+# See https://www.trueblade.com/blogs/news/python-3-10-new-dataclass-features
+@dataclass(kw_only=True)
+class SKOS:
     id_: str
     types: list[str]
     pref_labels: list[dict[str, str]]
-    schemes: list[dict]
     definitions: list[dict[str, str]] = field(default_factory=list)
-    notations: list[dict[str, str]] = field(default_factory=list)
-    alt_labels: list[dict[str, str]] = field(default_factory=list)
-    hidden_labels: list[dict[str, str]] = field(default_factory=list)
     change_notes: list[dict] = field(default_factory=list)
     history_notes: list[dict] = field(default_factory=list)
     editorial_notes: list[dict] = field(default_factory=list)
@@ -39,16 +38,16 @@ class Concept:
     def to_json_ld(self) -> dict:
         """Return this data formatted as (but not serialized to) SKOS expanded JSON LD"""
         dct = copy(self.extra)
-        for attr, label in CONCEPT_MAPPING.items():
+        for attr, label in SKOS_MAPPING.items():
             if value := getattr(self, attr):
                 dct[label] = value
 
         return dct
 
-    @staticmethod
-    def from_json_ld(concept_dict: dict) -> "Concept":
+    @classmethod
+    def from_json_ld(cls, concept_dict: dict) -> "Concept":
         source_dict, data = copy(concept_dict), {}
-        for dataclass_label, skos_label in CONCEPT_MAPPING.items():
+        for dataclass_label, skos_label in SKOS_MAPPING.items():
             if skos_label in source_dict:
                 data[dataclass_label] = source_dict.pop(skos_label)
         data["extra"] = {
@@ -56,11 +55,27 @@ class Concept:
             for key, value in source_dict.items()
             if key not in SKOS_RELATIONSHIP_PREDICATES
         }
-        return Concept(**data)
+        return cls(**data)
 
 
+@dataclass(kw_only=True)
+class Concept(SKOS):
+    schemes: list[dict]
+    notations: list[dict[str, str]] = field(default_factory=list)
+    alt_labels: list[dict[str, str]] = field(default_factory=list)
+    hidden_labels: list[dict[str, str]] = field(default_factory=list)
+
+
+@dataclass(kw_only=True)
+class ConceptScheme(SKOS):
+    created: list[datetime]
+    creators: list[dict]
+    version: list[str]
+
+
+# For type hinting
 # Will be Concept | ConceptScheme | Correspondence | Association
-GraphObject = Concept
+GraphObject = Concept | ConceptScheme
 
 
 class NotFoundError(Exception):
