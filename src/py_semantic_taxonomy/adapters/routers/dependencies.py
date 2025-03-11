3d7d1b983833ac65161cf3b7001af56f7a708760
Chris Mutel <cmutel@gmail.com>
2025-03-11 12:28:39 +0100

    fixup! Refactor engine creation and fix tests

--- src/py_semantic_taxonomy/adapters/routers/dependencies.py
+++ src/py_semantic_taxonomy/adapters/routers/dependencies.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 
 
-@lru_cache(size=1)
+@lru_cache(maxsize=1)
 def get_kos_graph():
     # Import inside function so we don't import any database-related stuff in tests
     from py_semantic_taxonomy.adapters.persistence.graph import PostgresKOSGraph
